dccb098c44d0bd8858599aac4d4f9c5f335c76f1
Nikhil-Singh-Bhadoriya <your-email@gmail.com>
2026-02-08 18:11:48 +0530

    Add complete sensor fault detection project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nikhil",
+    author_email="nb4047051@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+MONGO_DATABASE_NAME = "Nikhil"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://Nikhil:Nikhil@sensor.8mfc12o.mongodb.net/?appName=sensor"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
